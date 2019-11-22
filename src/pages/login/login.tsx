5752d63ef0f71584b8f1c53d739f9e6adcfdd576
Sol Nur <i.solnur@gmail.com>
2019-11-22 15:11:21 +0600

    added: sass support

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from 'src/pages/login/login.module.scss';
+
+export const Login: React.FC = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.formContainer}>
+
+            </div>
+        </div>
+    );
+};
